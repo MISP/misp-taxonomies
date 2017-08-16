05ce75f7fec34c9fa94a0a89b55695ae3f2c4211
Raphaël Vinot <raphael@vinot.info>
2017-08-16 17:39:58 +0800

    Update manifest

--- validate_all.sh
+++ validate_all.sh
@@ -12,6 +12,14 @@ if ! [ $diffs -eq 0 ]; then
 	exit 1
 fi
 
+directories=`ls -d */ | wc -w`
+manifest_entries=`cat MANIFEST.json | jq '.taxonomies | length'`
+
+if ! [ $directories -eq $manifest_entries ]; then
+    echo "MANIFEST isn't up-to-date."
+    exit 1
+fi
+
 for dir in */machinetag.json
 do
   echo -n "${dir}: "
