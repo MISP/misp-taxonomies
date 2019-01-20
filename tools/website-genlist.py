d17905805901285d75287a812bd37955b2c75f2d
Alexandre Dulaunoy <a@foo.be>
2019-01-20 11:11:32 +0100

    chg: [tools] replace function fixed

--- tools/website-genlist.py
+++ tools/website-genlist.py
@@ -6,4 +6,4 @@ with open(filename) as fp:
 	t = json.load(fp)
 
 for taxo in sorted(t['taxonomies'], key=lambda k: k['name']):
-    print ("[{}](https://github.com/MISP/misp-taxonomies/tree/master/{}):\n: {}[HTML](https://www.misp-project.org/taxonomies.html#_{})\n".format(taxo['name'], taxo['name'], taxo['description'], re.sub(r'_', '-',taxo['name'])))
+    print ("[{}](https://github.com/MISP/misp-taxonomies/tree/master/{}):\n: {}[HTML](https://www.misp-project.org/taxonomies.html#_{})\n".format(taxo['name'], taxo['name'], taxo['description'], re.sub(r'-', '_',taxo['name'])))
